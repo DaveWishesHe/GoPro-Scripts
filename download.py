8093bc30fa455d276f2e7205a08fb024c9151a13
Dave Stockdale <dave.g.stockdale@gmail.com>
2015-02-22 19:42:31 +0000

    Add handling of flags and arguments
    Functional:
    	-d / --destination to set directory to download to
        -e / --extension to specify file extension we're looking for
    
    Todo:
    	-h / --help to help the user out
        -v / --verbose to add a progress bar

--- download.py
+++ download.py
@@ -1,13 +1,46 @@
-import urllib
+import urllib, sys, getopt
 from lxml import html
 
-destination = "/home/dave/Pictures/2015-02/"
+def main(argv):
+    url = "http://10.5.5.9:8080/videos/DCIM/100GOPRO/"
+    destination = "/home/dave/"
+    extension = ".MP4"
+    try:
+        opts, args = getopt.getopt(argv, "hvd:e:", ["help", "verbose", "destination=", "extension="])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-url = "http://10.5.5.9:8080/videos/DCIM/100GOPRO/"
-page = html.fromstring(urllib.urlopen(url).read())
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-v", "--verbose"):
+            global _verbose
+            _verbose = 1
+	elif opt in ("-d", "--destination"):
+            destination = arg
+        elif opt in ("-e", "--extension"):
+            extension = arg
+        
+    page = html.fromstring(urllib.urlopen(url).read())
+    for link in page.xpath("//a"):
+        file = url + link.get("href")
+        if file.endswith(extension):
+            basic_download(file, destination + link.text)
 
-for link in page.xpath("//a"):
-    file = url + link.get("href")
-    if file.endswith(".MP4"):
-        print "Downloading", file, "To", destination + link.text 
-        urllib.urlretrieve(file, destination + link.text)
+# TODO: Implement user help
+def usage():
+      print "Huh?"
+
+def basic_download(source, destination):
+    print "Downloading", source, "To", destination
+    urllib.urlretrieve(source, destination)
+
+# TODO: Implement swanky progress bar
+def verbose_download(source, destination):
+    # Check this out: http://stackoverflow.com/a/22776
+    print "Not Implemented."
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
